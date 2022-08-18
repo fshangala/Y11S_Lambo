12e58799b3890f7e8ce974feda8e689418b6a1d9
fshangala <fshangala@gmail.com>
2022-08-18 06:54:12 +0200

    Initial commit

---
+++ app/src/main/java/com/fshangala/y11slambo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fshangala.y11slambo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
